4461c8a32a8d67118f4b14d3aefef6b3f9f139f5
Naan <naanko2021@gmail.com>
2022-02-04 12:55:15 +0300

    Reformat code

--- src/main/java/domain/Radio.java
+++ src/main/java/domain/Radio.java
@@ -5,55 +5,58 @@ public class Radio {
     private int currentStation;
     private int currentVolume;
 
-   public int getCurrentStation() {
-      return currentStation;
-   }
-   public void setCurrentStation(int currentStation) {
-      if (currentStation > 9) {
-         return;
-      }
-      if (currentStation < 0) {
-         return;
-      }
-      this.currentStation = currentStation;
-   }
-   public void setNext() {
-      if (currentStation < 9) {
-          currentStation++;
-      }
-         else  {
-             currentStation = 0;
-      }
-   }
-   public void setPrev() {
-      if (currentStation > 0) {
-          currentStation--;
-      }
-      else {
-          currentStation = 9;
-      }
-   }
-   public int getCurrentVolume() {
-      return currentVolume;
-   }
-   public void setCurrentVolume(int currentVolume) {
-      this.currentVolume = currentVolume;
-   }
-   public void increaseVolume() {
-      if (currentVolume < 10) {
-         currentVolume++;
-      }
-      else {
-         currentVolume = 10;
-      }
-   }
-   public void decreaseVolume() {
-      if (currentVolume > 0) {
-         currentVolume--;
-      }
-     else {
-         currentVolume = 0;
-      }
-   }
+    public int getCurrentStation() {
+        return currentStation;
+    }
+
+    public void setCurrentStation(int currentStation) {
+        if (currentStation > 9) {
+            return;
+        }
+        if (currentStation < 0) {
+            return;
+        }
+        this.currentStation = currentStation;
+    }
+
+    public void setNext() {
+        if (currentStation < 9) {
+            currentStation++;
+        } else {
+            currentStation = 0;
+        }
+    }
+
+    public void setPrev() {
+        if (currentStation > 0) {
+            currentStation--;
+        } else {
+            currentStation = 9;
+        }
+    }
+
+    public int getCurrentVolume() {
+        return currentVolume;
+    }
+
+    public void setCurrentVolume(int currentVolume) {
+        this.currentVolume = currentVolume;
+    }
+
+    public void increaseVolume() {
+        if (currentVolume < 10) {
+            currentVolume++;
+        } else {
+            currentVolume = 10;
+        }
+    }
+
+    public void decreaseVolume() {
+        if (currentVolume > 0) {
+            currentVolume--;
+        } else {
+            currentVolume = 0;
+        }
+    }
 }
 
